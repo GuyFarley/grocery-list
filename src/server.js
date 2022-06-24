4e1b59854c77afedd41488463ff51ad9f35a6412
Guy Farley <guy.e.farley@gmail.com>
2022-06-23 18:10:09 -0700

    Modified API portion of server for grocery list

--- src/server.js
+++ src/server.js
@@ -1,32 +1,21 @@
 'use strict';
 
-// 3rd Party Resources
 const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-
-// Esoteric Resources
-const errorHandler = require('./error-handlers/500.js');
-const notFound = require('./error-handlers/404.js');
-const authRoutes = require('./auth/router/routes.js');
+const errorHandler = require('./error-handlers/500');
+const notFoundHandler = require('./error-handlers/404');
+const itemRouter = require('./routes/list-item');
 
 const app = express();
 
-app.use(cors());
-app.use(morgan('dev'));
+const PORT = process.env.PORT || 3002;
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(authRoutes);
-
-app.use('*', notFound);
+app.use(itemRouter);
+app.use(notFoundHandler);
 app.use(errorHandler);
+
 
 module.exports = {
   server: app,
-  start: port => {
-    if (!port) { throw new Error('Missing Port'); }
-    app.listen(port, () => console.log(`Listening on ${port}`));
-  },
+  start: () => app.listen(PORT, console.log('listening on port', PORT)),
 };
