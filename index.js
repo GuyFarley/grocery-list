894c3205bec63d856c48e7649f1c926850f854ce
Marcus Hartwig <marcusdnhartwig@gmail.com>
2022-06-26 16:42:58 -0700

    dontmerge

--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const { db } = require('./src/auth/models/index.js');
-const server = require('./src/server.js');
-
-db.sync().then(() => {
-  server.start(process.env.PORT || 3002);
-});
