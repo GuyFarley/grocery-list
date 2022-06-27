23440bbebb369f0a29d487d034c6d0d24fb56d67
Guy Farley <guy.e.farley@gmail.com>
2022-06-27 08:01:46 -0700

    Added code to sequelize variable for heroku postgres deployment

--- src/models/index.js
+++ src/models/index.js
@@ -8,7 +8,14 @@ const DATABASE_URL = process.env.NODE_ENV === 'test'
   ? 'sqlite::memory'
   : process.env.DATABASE_URL || 'sqlite:memory';  // one colon allows us to persist
 
-const sequelize = new Sequelize(DATABASE_URL);
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 module.exports = {
   sequelize,
