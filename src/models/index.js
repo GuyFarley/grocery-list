7e27260bd624595c9f8d0971a5f8c4ae099d5c44
Guy Farley <guy.e.farley@gmail.com>
2022-06-27 08:05:24 -0700

    Added code to models/index for heroku deployment

--- src/models/index.js
+++ src/models/index.js
@@ -4,9 +4,17 @@ const { Sequelize, DataTypes } = require('sequelize');
 const userSchema = require('./user-model');
 const itemSchema = require('./item-model');
 
+// use this code locally
+// const DATABASE_URL = process.env.NODE_ENV === 'test'
+//   ? 'sqlite::memory'
+//   : process.env.DATABASE_URL || 'sqlite:memory';  // one colon allows us to persist
+
+// const sequelize = new Sequelize(DATABASE_URL)
+
+// this code for deployment
 const DATABASE_URL = process.env.NODE_ENV === 'test'
   ? 'sqlite::memory'
-  : process.env.DATABASE_URL || 'sqlite:memory';  // one colon allows us to persist
+  : process.env.DATABASE_URL || 'postgres://localhost:5432/grocery-list';
 
 const sequelize = new Sequelize(DATABASE_URL, {
   dialectOptions: {
